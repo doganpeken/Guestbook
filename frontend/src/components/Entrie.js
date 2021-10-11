09ad8829f3ffff2fc73c6e8646bf50dd082b5886
Dogan Peken <dogan.peken@antwork.io>
2021-10-11 11:29:45 +0200

    changed env

--- frontend/src/components/Entrie.js
+++ frontend/src/components/Entrie.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-const host = process.env.ENDPOINT;
+const host = "http://localhost:3000";
 
 function Entrie({ setView }) {
   const [name, setName] = useState("");
