a8ef099f882cf02c56d0bd25b2dd13c0224e6fa6
Dogan Peken <dogan.peken@antwork.io>
2021-10-11 14:50:33 +0200

    test

--- frontend/src/components/EntrieList.js
+++ frontend/src/components/EntrieList.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-const host = "http://localhost:3000";
+const host = "http://localhost";
 
 function EntrieList({ view, setView }) {
   const [entries, setEntries] = useState([]);
