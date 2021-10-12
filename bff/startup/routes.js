2ac00202bd162a140c921356bfddc7c29da96309
Dogan Peken <dogan.peken@antwork.io>
2021-10-12 18:21:28 +0200

    test

--- bff/startup/routes.js
+++ bff/startup/routes.js
@@ -8,12 +8,14 @@ const cors = require("cors");
 module.exports = function (app) {
   app.use(cors());
   app.use(express.json());
+
   app.use(express.static("../frontend/build"));
   app.use(express.json());
   app.use("/guestbook/entries", entries);
+
   app.get("/", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../../", "frontend/build", "/index.html")
+      path.join(__dirname, "..", "..", "frontend", "build", "index.html")
     );
   });
 };
