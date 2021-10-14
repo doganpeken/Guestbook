fed01e2dcc097a2d1aacf5378fc26c3d0d2abc4f
Dogan Peken <dogan.peken@antwork.io>
2021-10-14 12:21:19 +0200

    changed hostname

--- bff/routes/entries.js
+++ bff/routes/entries.js
@@ -6,24 +6,9 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", async (req, res) => {
-  // res.send({ message: `${restAPI}` });
-  const response = await axios.get(
-    "http://nginx-guestbookapi/guestbook/entries"
-  );
-  res.send(response.data);
-});
-
-router.get("/test", async (req, res) => {
-  // res.send({ message: `${restAPI}` });
   const response = await axios.get(
     "http://guestbookapi-service/guestbook/entries"
   );
-  res.send(response.data);
-});
-
-router.get("/test2", async (req, res) => {
-  // res.send({ message: `${restAPI}` });
-  const response = await axios.get("http://20.79.225.113/guestbook/entries");
   res.send(response.data);
 });
 
@@ -33,10 +18,8 @@ router.post("/", async (req, res) => {
     res.send("Bad request.").status(400);
     return;
   }
-  // let now = new Date();
-  // entrie.created = now.toISOString();
   const response = await axios.post(
-    "http://10.0.117.173/guestbook/entries",
+    "http://guestbookapi-service/guestbook/entries",
     entrie
   );
   res.status(response.status).send({ message: "OK" });
